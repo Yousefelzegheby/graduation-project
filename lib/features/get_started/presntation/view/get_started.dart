b1fe73f99f163a7823282d6ed838aae5af8ab052
ENG-yousef-elzegheby <yousef44alzegheby@gmail.com>
2023-12-07 20:50:01 +0200

    get start listView

--- lib/features/get_started/presntation/view/get_started.dart
+++ lib/features/get_started/presntation/view/get_started.dart
@@ -10,33 +10,35 @@ class GetStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 80),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                AssetsPath.logo,
-                height: MediaQuery.of(context).size.height * .22,
-              ),
-              Image.asset(
-                AssetsPath.person,
-                height: MediaQuery.of(context).size.height * .50,
-              ),
-              CustomButton(
-                textColor: AppColors.kLightColor,
-                fontsize: 16,
-                theText: 'GET STARTED',
-                onpressed: () {
-                  GoRouter.of(context).push('/asDoctorOrStudent');
-                },
-                backgroundColor: AppColors.kPrimary,
-              ),
-            ],
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 80),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  AssetsPath.logo,
+                  height: MediaQuery.of(context).size.height * .22,
+                ),
+                Image.asset(
+                  AssetsPath.person,
+                  height: MediaQuery.of(context).size.height * .50,
+                ),
+                CustomButton(
+                  textColor: AppColors.kLightColor,
+                  fontsize: 16,
+                  theText: 'GET STARTED',
+                  onpressed: () {
+                    GoRouter.of(context).push('/asDoctorOrStudent');
+                  },
+                  backgroundColor: AppColors.kPrimary,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
